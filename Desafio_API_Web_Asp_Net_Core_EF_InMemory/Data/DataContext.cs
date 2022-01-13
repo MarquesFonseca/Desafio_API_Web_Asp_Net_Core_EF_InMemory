8bfffb8668e08c5786dbab56a8d5f3a8af171e0a
Marques Silva Fonseca <marques-fonseca@hotmail.com>
2022-01-13 15:54:30 -0300

    Adicionando o nuget Microsoft.EntityFrameworkCore.InMemory

---
+++ Desafio_API_Web_Asp_Net_Core_EF_InMemory/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Desafio_API_Web_Asp_Net_Core_EF_InMemory.Data
+{
+    public class DataContext
+    {
+
+    }
+}
