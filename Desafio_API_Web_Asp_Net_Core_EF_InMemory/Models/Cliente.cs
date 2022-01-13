2f30364eb0c272de24baa4a14ab859d474a82c71
Marques Silva Fonseca <marques-fonseca@hotmail.com>
2022-01-13 15:15:51 -0300

    Adicionado Model Estado.cs

---
+++ Desafio_API_Web_Asp_Net_Core_EF_InMemory/Models/Cliente.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Desafio_API_Web_Asp_Net_Core_EF_InMemory.Models
+{
+    public class Cliente
+    {
+    }
+}
---
+++ Desafio_API_Web_Asp_Net_Core_EF_InMemory/Models/Estado.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Desafio_API_Web_Asp_Net_Core_EF_InMemory.Models
+{
+    public class Estado
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [MaxLength(100, ErrorMessage = "Este campo deve conter entre 2 e 100 caracteres")]
+        [MinLength(2, ErrorMessage = "Este campo deve conter entre 2 e 100 caracteres")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [MaxLength(2, ErrorMessage = "Este campo deve conter 2 caracteres")]
+        [MinLength(2, ErrorMessage = "Este campo deve conter 2 caracteres")]
+        public string Uf { get; set; }
+    }
+}
