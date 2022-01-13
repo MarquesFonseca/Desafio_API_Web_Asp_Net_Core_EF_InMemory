857171bc66693392cec6cf3df613ab789332d414
Marques Silva Fonseca <marques-fonseca@hotmail.com>
2022-01-13 15:25:10 -0300

    Adicionado Model Cidade.cs

--- Desafio_API_Web_Asp_Net_Core_EF_InMemory/Models/Cliente.cs
+++ Desafio_API_Web_Asp_Net_Core_EF_InMemory/Models/Cliente.cs
@@ -2,10 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Desafio_API_Web_Asp_Net_Core_EF_InMemory.Models
 {
     public class Cliente
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório.")]
+        [MaxLength(100, ErrorMessage = "Este campo deve conter entre 2 e 100 caracteres.")]
+        [MinLength(2, ErrorMessage = "Este campo deve conter entre 2 e 100 caracteres.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório.")]
+        [Range(1, int.MaxValue, ErrorMessage ="Estado inválido."]
+        public string EstadoId { get; set; }
+        public Estado Estado { get; set; }
     }
 }
--- Desafio_API_Web_Asp_Net_Core_EF_InMemory/Models/Estado.cs
+++ Desafio_API_Web_Asp_Net_Core_EF_InMemory/Models/Estado.cs
@@ -11,14 +11,14 @@ namespace Desafio_API_Web_Asp_Net_Core_EF_InMemory.Models
         [Key]
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "Este campo é obrigatório")]
-        [MaxLength(100, ErrorMessage = "Este campo deve conter entre 2 e 100 caracteres")]
-        [MinLength(2, ErrorMessage = "Este campo deve conter entre 2 e 100 caracteres")]
+        [Required(ErrorMessage = "Este campo é obrigatório.")]
+        [MaxLength(100, ErrorMessage = "Este campo deve conter entre 2 e 100 caracteres.")]
+        [MinLength(2, ErrorMessage = "Este campo deve conter entre 2 e 100 caracteres.")]
         public string Nome { get; set; }
 
-        [Required(ErrorMessage = "Este campo é obrigatório")]
-        [MaxLength(2, ErrorMessage = "Este campo deve conter 2 caracteres")]
-        [MinLength(2, ErrorMessage = "Este campo deve conter 2 caracteres")]
+        [Required(ErrorMessage = "Este campo é obrigatório.")]
+        [MaxLength(2, ErrorMessage = "Este campo deve conter 2 caracteres.")]
+        [MinLength(2, ErrorMessage = "Este campo deve conter 2 caracteres.")]
         public string Uf { get; set; }
     }
 }
