88fdffa2ebb6a1000ac4e035040debf4dee0bf92
Marques Silva Fonseca <marques-fonseca@hotmail.com>
2022-01-13 16:13:30 -0300

    Configurando o nosso Startup.cs

--- Desafio_API_Web_Asp_Net_Core_EF_InMemory/Startup.cs
+++ Desafio_API_Web_Asp_Net_Core_EF_InMemory/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Desafio_API_Web_Asp_Net_Core_EF_InMemory.Data;
 
 namespace Desafio_API_Web_Asp_Net_Core_EF_InMemory
 {
@@ -22,9 +23,20 @@ namespace Desafio_API_Web_Asp_Net_Core_EF_InMemory
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        /// <summary>
+        /// Informar para nossa aplicação que temos um DataContext,
+        /// e que vamos trabalhar com o Entity Framework,
+        /// e que usaremos nosso banco de dados em memória.
+        /// Estamos dando o nome para o nosso banco de dados de "DataBase"
+        /// </summary>
+        /// <param name="services"></param>
         public void ConfigureServices(IServiceCollection services)
         {
+            //Deixar nosso DatContext disponível através do "AddScoped", que é a nossa injeção de dependências do .net Core.
+            //Significa que, se em algum lugar da nossa aplicação solicitar o DataContext, vai deixar em memória,
+            //onde não cria uma nova versão toda vez que requisitar, ou seja, não vai abri uma nova conexão no banco.
+            //e assim que a requisição terminar, a aplicação vai destruir o DataContext, para não deixar vestígios na memória.
+            services.AddScoped<DataContext, DataContext>();
             services.AddControllers();
         }
 
