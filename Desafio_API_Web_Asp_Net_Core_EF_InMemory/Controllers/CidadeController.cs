889fb460df54c598ebf53ee396813bf4fc390744
Marques Silva Fonseca <marques-fonseca@hotmail.com>
2022-01-13 19:36:57 -0300

    Update controller CidadeController.cs

--- Desafio_API_Web_Asp_Net_Core_EF_InMemory/Controllers/CidadeController.cs
+++ Desafio_API_Web_Asp_Net_Core_EF_InMemory/Controllers/CidadeController.cs
@@ -9,17 +9,17 @@ using Desafio_API_Web_Asp_Net_Core_EF_InMemory.Models;
 
 namespace Desafio_API_Web_Asp_Net_Core_EF_InMemory.Controllers
 {
-    [ApiController]
-    [Route("cidade")]
-    public class CidadeController : Controller
+    [ApiController] //definindo para a classe Contoller, que usaremos o ApiController
+    [Route("cidade")] //Como não definimos nenhuma rota no endPoints(Startup.cs), o mapeamento das rotas serão pela rotas do controller atual
+    public class CidadeyController : ControllerBase
     {
         /// <summary>
-        /// Retorna uma lista de cidade, usando o Task de forma Assíncrona.
+        /// Retorna uma lista de Cidades, usando o Task de forma assíncrona. 
         /// </summary>
-        /// <param name="dataContext">Acessa os dados. O "[FromServices"] indica que vai utilizar o DataContect que já está na memória</param>
-        /// <returns>Retorna toda lista de Cidades</returns>
-        [HttpGet]//definindo o verbo utilizado. Se não colocar nada, por padrão ele já usa o GET
-        [Route("")]//Rota vazia, ou seja, será a mesa rota definida no controller.
+        /// <param name="dataContext">Acessar os dados. o "[FromServices]" indica que vai utilizar o DataContext que já está em memória</param>
+        /// <returns>Retorna uma lista de Cidades</returns>
+        [HttpGet]//definindo o verbo utilizado. Se não colocar nada, por padrão ele assume o GET
+        [Route("")]//rota vazia, ou seja, será a mesma rota definida no controller
         public async Task<ActionResult<List<Cidade>>> Get([FromServices] DataContext dataContext)
         {
             var cidades = await dataContext.Cidades.AsNoTracking().ToListAsync();
@@ -27,19 +27,140 @@ namespace Desafio_API_Web_Asp_Net_Core_EF_InMemory.Controllers
         }
 
         /// <summary>
-        /// Retorna uma lista de cidade, em ordem Alfabética.
+        /// Retorna uma Lista de Cidades em ordem Decrescente.
         /// É um método privado, usado internamente.
         /// </summary>
-        /// <param name="dataContext">Acessa os dados. O "[FromServices"] indica que vai utilizar o DataContect que já está na memória</param>
-        /// <returns>Retorna toda lista de Cidades em orde A-Z</returns>
+        /// <param name="dataContext">Acessar os dados. o "[FromServices]" indica que vai utilizar o DataContext que já está em memória</param>
+        /// <returns>Retorna uma lista de Cidades em ordem Descrescente</returns>
         [HttpGet]
         [Route("")]
-        private async Task<ActionResult<List<Cidade>>> GetOrderByCrescente([FromServices] DataContext dataContext)
+        private async Task<ActionResult<List<Cidade>>> GetOrderByDesc([FromServices] DataContext dataContext)
         {
-            var cidades = await dataContext.Cidades.AsNoTracking().OrderBy(x => x.Nome).ToListAsync();
+            var cidades = await dataContext.Cidades.AsNoTracking().OrderByDescending(x => x.Id).ToListAsync();
             return cidades;
         }
 
+        /// <summary>
+        /// Retorna uma Cidade informando o Id da Cidade desejada
+        /// </summary>
+        /// <param name="context">Acessar os dados. o "[FromServices]" indica que vai utilizar o DataContext que já está em memória</param>
+        /// <param name="id">Informe o Id da Cidade desejado</param>
+        /// <returns>Retorna uma Cidade</returns>
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<ActionResult<Cidade>> GetById([FromServices] DataContext context, int id)
+        {
+            var cidade = await context.Cidades.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+            return cidade;
+        }
 
+        /// <summary>
+        /// Verifica se o Id da Cidade existe na base de dados.
+        /// Método privado, só acessa internamente.
+        /// </summary>
+        /// <param name="context">Acessar os dados. o "[FromServices]" indica que vai utilizar o DataContext que já está em memória</param>
+        /// <param name="id">Informe o Id da Cidade desejado.</param>
+        /// <returns>Retorna "Verdadeiro / Falso"</returns>
+        [HttpGet]
+        [Route("{id:int}")]
+        private async Task<ActionResult<bool>> VerificaSeExiste([FromServices] DataContext context, int id)
+        {
+            bool existe = await context.Cidades.AnyAsync(x => x.Id == id);
+            return existe;
+        }
+
+        /// <summary>
+        /// Grava na base uma nova Cidade.
+        /// E retorna toda a lista das Cidades já cadastradas.
+        /// </summary>
+        /// <param name="context">Representação do banco de dados em memória</param>
+        /// <param name="model">Modelo via json passado pelo Body</param>
+        /// <returns>Retorna uma lista de todos.</returns>        
+        [HttpPost]
+        [Route("")]
+        public async Task<ActionResult<List<Cidade>>> Post([FromServices] DataContext context, [FromBody] Cidade model)
+        {
+            if (ModelState.IsValid)
+            {
+                //System.Guid g = System.Guid.NewGuid();
+                //model.Id = g.ToString();
+                context.Cidades.Add(model);
+                await context.SaveChangesAsync();
+                return await GetOrderByDesc(context);
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+
+        /// <summary>
+        /// Altera os dados da Cidade atual
+        /// </summary>
+        /// <param name="context">Representação do banco de dados</param>
+        /// <param name="model">Dados a ser alterado no modelo json pelo body</param>
+        /// <param name="id">Informe o Id da Cidade desejada</param>
+        /// <returns>Retorna a Cidade já alterada.</returns>
+        [HttpPut("{id}")]
+        [Route("alterar/{id:int}")]
+        public async Task<ActionResult<Cidade>> Alterar([FromServices] DataContext context, [FromBody] Cidade model, int id)
+        {
+            if (model.Id != id)
+            {
+                return BadRequest();
+            }
+
+            bool existe = await context.Cidades.AnyAsync(x => x.Id == id);
+            if (!existe)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                context.Entry(model).State = EntityState.Modified;
+                await context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                var cidade = await context.Cidades.FindAsync(id);
+                if (cidade == null)
+                {
+                    //return BadRequest();
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            var retorno = await context.Cidades.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+            return retorno;
+        }
+
+        /// <summary>
+        /// Deleta/Remove a Cidade repassando via "Rota" o id desejado para exclusão
+        /// </summary>
+        /// <param name="context">Representação do nosso banco de dados</param>
+        /// <param name="id">Informe o Id desejado para exclusão</param>
+        /// <returns>Não retorna nada.</returns>
+        [HttpDelete("{id}")]
+        [Route("deletar/{id:int}")]
+        public async Task<IActionResult> Deletar([FromServices] DataContext context, int id)
+        {
+            var cidadeAtual = await context.Cidades.FindAsync(id);
+            if (cidadeAtual == null)
+            {
+                //return BadRequest();
+                return NotFound();
+            }
+
+            context.Cidades.Remove(cidadeAtual);
+            await context.SaveChangesAsync();
+
+            //return NoContent();
+            return Ok();
+        }
     }
 }
